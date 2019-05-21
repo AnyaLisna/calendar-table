2991be792d4b4c31eea9ace07cdf30ead80a94b9
AnyaLisna <anyalesnaya2812@gmail.com>
2019-05-21 14:42:03 +0300

    fix calendar

--- script.js
+++ script.js
@@ -1,45 +1,41 @@
+'use strict';
+
 function createCalendar(id, year, month) {
-    id = document.getElementById(id);
-    const table = document.createElement('table');
-    const tr = document.createElement('tr');
-    const td = document.createElement('td');
-    const weekDay = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
-  
-    const date = new Date(year, month - 1); // создает дату
-    const firstEmptyCell = date.getDay() - 2; // получаем число пустых ячеек до первого дня
-    const lastDayMonth = new Date(year, month, 0).getDate() + 1;  //кодичество дней в месяце
-  
-    // создаем и заполняем оглавление таблицы
-    id.appendChild(table);
-    table.appendChild(tr);
-    weekDay.forEach(dayName => {
-      const th = document.createElement('th');
-      tr.appendChild(th);
-      th.appendChild(document.createTextNode(dayName));
-    });
-  
-    // создаем строки
-    table.appendChild(tr.cloneNode(false));
-  
-    //создает ячейки и заполняем числами
-    for (let i = 1; i <= firstEmptyCell + lastDayMonth; i++) {
-      table.lastChild.appendChild(td.cloneNode(true));
-      td.innerHTML = (i <= firstEmptyCell ? '' : i - firstEmptyCell);
-  
-      //создаем переносы
-      if (i % 7 === 0) {
-        table.appendChild(tr.cloneNode(false));
+
+  const table = document.createElement('table');
+  const tr = document.createElement('tr');
+  const weekDay = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+
+  const date = new Date(year, month - 1); // создает дату
+  let firstDayMonth = date.getDay(); // получаем число пустых ячеек до первого дня
+  firstDayMonth = firstDayMonth === 0 ? 6 : firstDayMonth - 1;
+  const lastDayMonth = new Date(year, month, 0).getDate(); //количество дней в месяце
+
+  // создаем и заполняем оглавление таблицы
+  document.getElementById(id).appendChild(table);
+  table.appendChild(tr);
+  weekDay.forEach(dayName => {
+    const th = document.createElement('th');
+    tr.appendChild(th);
+    th.appendChild(document.createTextNode(dayName));
+  });
+
+  //создает ячейки и заполняем числами
+
+  let day = 1 - firstDayMonth;
+
+  while (day <= lastDayMonth) {
+    const week = document.createElement('tr');
+    for (let i = 0; i < 7; i++) {
+      const td = document.createElement('td');
+      if (day > 0 && day <= lastDayMonth) {
+        td.innerHTML = day;
       }
+      day++;
+      week.append(td);
     }
-  
-    // узнаём недостающие пустые ячейки
-    const lastEmptyCell = 7 - table.lastChild.childNodes.length;
-  
-    // добавляем пустые ячейки в конце
-    for (let j = 0; j < lastEmptyCell; j++) {
-      table.lastChild.appendChild(td.cloneNode(false));
-    }
+    table.append(week);
   }
-  
-  createCalendar('cal', 2019, 2);
-  
+}
+
+createCalendar('cal', 2019, 9);
